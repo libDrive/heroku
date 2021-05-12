ef8968a47f3c4206f00c00e8f53c4a73ec511f8d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-05-12 21:49:28 +0400

    Removed auto_pinger because it doesn't seem to work

--- start.sh
+++ start.sh
@@ -22,26 +22,4 @@ curl -L -s $(curl -s "https://api.github.com/repos/${REPO}/releases/${VER}" | gr
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
-get_host() {
-	local host
-	host=$(hostname -A)
-	echo "$host:$PORT" | tr -d " "
-}
-
-auto_pinger() {
-	host=$(get_host)
-    gunicorn main:app &
-	P1=$!
-	while :; do
-		sleep 600
-		curl -s "$host/api/v1/ping"
-	done &
-	P2=$!
-	wait $P1 $P2
-}
-
-if [ "${LIBDRIVE_PINGER}" = "true" ]; then
-	auto_pinger
-else
-    gunicorn main:app
-fi
+gunicorn main:app
