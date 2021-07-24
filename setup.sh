9c4ffd012c319d0f8a014b3912f5ca6ca4b91a4e
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-07-24 14:04:34 +0400

    Updated setup.sh for changes to server folder structure

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ git clone "https://github.com/libDrive/web.git" --depth 1 web
 mkdir ./libDrive.Server
 mkdir ./libDrive.Server/build
 mkdir ./libDrive.Server/src
+mkdir ./libDrive.Server/templates
 cd ./web
 yarn install
 yarn run build
@@ -18,6 +19,7 @@ mv ./build/* ../libDrive.Server/build
 cd ../server
 mv main.py requirements.txt ../libDrive.Server
 mv ./src/* ../libDrive.Server/src
+mv ./templates/* ../libDrive.Server/templates
 cd ..
 cp ../../README.md ../../LICENSE ./libDrive.Server
 cd libDrive.Server
